3b56a4de91271a5115b0a3dda99cdbdfbb2a9830
Mike <andreaem@users.noreply.github.com>
2019-10-10 15:05:33 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
+echo "SET wifi first!!! until the script is fixed"
+
+echo "this shouldn't be run in sudo just fyi"
+
+echo "choose a password"
+passwd
+
 sudo apt install git
 
 git clone https://github.com/andreaem/Bar-Sign.git
 
 sudo apt-get install python3-rpi.gpio
 
-#get userinfo for name and password
-sudo echo 'country=CA' >> /etc/wpa_supplicant/wpa_supplicant.conf
-sudo echo 'network={ ssid="testing" psk="testingPassword" }' >> /etc/wpa_supplicant/wpa_supplicant.conf
+#the sudo isn't working quite right...
+#echo "enter the SSID of the network"
+#read ssid
+#echo "enter the password of the network"
+#read pw
+#sudo echo 'country=CA' >> /etc/wpa_supplicant/wpa_supplicant.conf
+#sudo echo 'network={ ssid="$ssid" psk="$pw" }' >> /etc/wpa_supplicant/wpa_supplicant.conf
 
 sudo crontab ./crontab_config
 
