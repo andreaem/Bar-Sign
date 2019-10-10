158ed5f3709fbb35898f7f0074d3ac724cd978e9
Mike <andreaem@users.noreply.github.com>
2019-10-10 16:01:43 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ echo "SET wifi first!!! until the script is fixed"
 
 echo "this shouldn't be run in sudo just fyi"
 
-echo "choose a password"
+echo "choose a password, enter your old one first"
 passwd
 
 sudo apt install git
@@ -13,17 +13,17 @@ git clone https://github.com/andreaem/Bar-Sign.git
 
 sudo apt-get install python3-rpi.gpio
 
-sed -i '$ d' /etc/rc.local
-sudo echo "python3 /home/py/Bar-Sign/pythonSign.py" >> /etc/rc.local
-sudo echo "exit 0" >> /etc/rc.local
+sudo sed -i '$ d' /etc/rc.local
+sudo bash -c 'echo "python3 /home/py/Bar-Sign/pythonSign.py" >> /etc/rc.local'
+sudo bash -c 'sudo echo "exit 0" >> /etc/rc.local'
 
 #the sudo isn't working quite right...
 echo "enter the SSID of the network"
 read ssid
 echo "enter the password of the network"
 read pw
-sudo echo 'country=CA' >> /etc/wpa_supplicant/wpa_supplicant.conf
-sudo echo 'network={ ssid="$ssid" psk="$pw" }' >> /etc/wpa_supplicant/wpa_supplicant.conf
+echo 'country=CA' | sudo tee -a /etc/wpa_supplicant/wpa_supplicant.conf
+echo 'network={ ssid="$ssid" psk="$pw" }' | sudo tee -a /etc/wpa_supplicant/wpa_supplicant.conf
 
 #doesn't work on locked down system
 #sudo crontab ./crontab_config
